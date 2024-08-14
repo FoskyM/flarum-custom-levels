4da758de9a556807d2afb8c4cb418ae5063dad37
FoskyM <i@fosky.top>
2024-08-14 10:47:30 +0800

    fix: calculation error

--- js/src/forum/components/LevelBar.js
+++ js/src/forum/components/LevelBar.js
@@ -1,6 +1,7 @@
 import app from 'flarum/forum/app';
 import Component from 'flarum/common/Component';
 import Tooltip from 'flarum/common/components/Tooltip';
+import extractText from 'flarum/common/utils/extractText';
 
 export default class LevelBar extends Component {
   oninit(vnode) {
@@ -9,10 +10,13 @@ export default class LevelBar extends Component {
 
   view() {
     const user = this.attrs.user;
-    let levelText = app.forum.attribute('foskym-custom-levels.levelText') || app.translator.trans('foskym-custom-levels.lib.defaults.level')[0];
-    let expText = app.forum.attribute('foskym-custom-levels.expText') || app.translator.trans('foskym-custom-levels.lib.defaults.exp')[0];
-    let expFormula = app.forum.attribute('foskym-custom-levels.expFormula') || app.translator.trans('foskym-custom-levels.lib.defaults.expFormula')[0];
-    let levelFormula = app.forum.attribute('foskym-custom-levels.levelFormula') || app.translator.trans('foskym-custom-levels.lib.defaults.levelFormula')[0];
+    let levelText =
+      app.forum.attribute('foskym-custom-levels.levelText') || extractText(app.translator.trans('foskym-custom-levels.lib.defaults.level'));
+    let expText = app.forum.attribute('foskym-custom-levels.expText') || extractText(app.translator.trans('foskym-custom-levels.lib.defaults.exp'));
+    let expFormula =
+      app.forum.attribute('foskym-custom-levels.expFormula') || extractText(app.translator.trans('foskym-custom-levels.lib.defaults.expFormula'));
+    let levelFormula =
+      app.forum.attribute('foskym-custom-levels.levelFormula') || extractText(app.translator.trans('foskym-custom-levels.lib.defaults.levelFormula'));
 
     expFormula = expFormula
       .replace('[commentCount]', user.commentCount())
@@ -20,37 +24,44 @@ export default class LevelBar extends Component {
       .replace('[money]', 'antoinefr-money' in flarum.extensions ? user.attribute('money') || 0 : 0)
       .replace('[likesReceived]', 'clarkwinkelmann-likes-received' in flarum.extensions ? user.attribute('likesReceived') || 0 : 0)
       .replace('[bestAnswerCount]', 'fof-best-answer' in flarum.extensions ? user.attribute('bestAnswerCount') || 0 : 0);
-
-    let expTotal = Math.floor(eval(expFormula));
+ 
     let expLevel = 0;
     let expPercent = 0;
 
-    levelFormula = levelFormula
-        .replace('[expTotal]', expTotal);
+    let error = false;
 
-    if (levelFormula.indexOf('[level]') > -1) {
+    try {
+      let expTotal = Math.floor(eval(expFormula));
+
+      levelFormula = levelFormula.replace('[expTotal]', expTotal);
+      if (levelFormula.indexOf('[level]') > -1) {
         let n = 1;
         while (n < eval(levelFormula.replace('[level]', n))) {
-            n++;
+          n++;
         }
         expLevel = Math.floor(n - 1);
         expPercent = (eval(levelFormula.replace('[level]', expLevel)) - expLevel) * 100;
         if (expPercent > 100) expPercent = (eval(levelFormula.replace('[level]', expLevel)) - expLevel - 1) * 100;
-    } else {
+      } else {
         expLevel = Math.floor(eval(levelFormula));
         expPercent = (eval(levelFormula) - expLevel) * 100;
+      }
+
+      expText = expText.replace('[expTotal]', expTotal);
+      if (levelText.indexOf('[level]') > -1) levelText = levelText.replace('[level]', expLevel);
+      else levelText = levelText + ' ' + expLevel;
+    } catch (e) {
+      console.error(e);
+      error = true;
+      expText = extractText(app.translator.trans('foskym-custom-levels.lib.error.calculation'));
     }
-
-    expText = expText.replace('[expTotal]', expTotal);
-    if (levelText.indexOf('[level]') > -1) levelText = levelText.replace('[level]', expLevel);
-    else levelText = levelText + ' ' + expLevel;
-
-    return (
+    
+    return error ? (
+      <div class="CustomLevel-level">{expText}</div>
+    ) : (
       <Tooltip text={expText}>
         <div class="CustomLevel-level">
-          <span class="CustomLevel-text">
-            {levelText}
-          </span>
+          <span class="CustomLevel-text">{levelText}</span>
           <div class="CustomLevel-bar">
             <div class="CustomLevel-bar-value" style={'width: ' + expPercent + '%;'}></div>
           </div>
