4ecc918646a20365cdbe34157b6fa1f7822131d9
FoskyM <i@fosky.top>
2024-08-27 14:43:53 +0800

    feat with `nodeloc-leaderboard`

--- js/src/forum/integrations/index.js
+++ js/src/forum/integrations/index.js
@@ -2,6 +2,7 @@ import app from 'flarum/common/app';
 import { extend } from 'flarum/common/extend';
 import store from './store';
 import userDirectry from './userDirectory';
+import leaderboard from './leaderboard';
 
 export default function () {
   if ('xypp-store' in flarum.extensions) {
@@ -9,5 +10,8 @@ export default function () {
   }
   if ('fof-user-directory' in flarum.extensions) {
     userDirectry(app);
+  }
+  if ('nodeloc-leaderboard' in flarum.extensions) {
+    leaderboard(app);
   }
 }
